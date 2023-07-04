1812bd87b719273d5aae90999ea9e325ebc537e9
prayagupa <prayag.upd@gmail.com>
2023-07-03 17:09:07 -0700

    plot trained and forecasted clicks

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 
-https://www.coursera.org/learn/ml-foundations
+```bash
+## https://matplotlib.org/stable/users/installing/index.html
+pip install matplotlib
 
+python plot-ad-clicks.py
+```
+
+Resources
+---------
+
+- https://www.coursera.org/learn/ml-foundations
+- https://www.coursera.org/learn/ml-regression
--- plot-ad-clicks.py
+++ plot-ad-clicks.py
@@ -1,27 +1,52 @@
 import matplotlib.pyplot as plt
 import csv
+import numpy as np
 
-training_dict = csv.DictReader(open('src/main/resources/ad_click_data_train.csv', 'r'), 
+training_dict = csv.DictReader(open('src/main/resources/ad_click_data_train.csv', 'r'),
         fieldnames = ['date', 'impressions', 'clicks', 'ctr'], delimiter = ',', quotechar = '"')
 
-dict = csv.DictReader(open('src/main/resources/ad_click_data.csv', 'r'), 
+testing_dict = csv.DictReader(open('src/main/resources/ad_click_data.csv', 'r'),
         fieldnames = ['date', 'impressions', 'clicks', 'ctr'], delimiter = ',', quotechar = '"')
 i = 1
-x = []
-y = []
+train_x = []
+train_y = []
+test_y = []
+
 for row in training_dict:
     if row['date']!='date' or row['impressions']!='impressions':
         print(str(i) + ": " + row['impressions'])
-        x.append(i)
-        y.append(row['impressions'])
+        train_x.append(i)
+        train_y.append(int(row['impressions']))
         i = i + 1
-  
-plt.plot(x, y)
-  
+
+for row in testing_dict:
+  if row['date']!='date' or row['impressions']!='impressions':
+    print(str(i) + ": " + row['impressions'])
+    train_x.append(i)
+    test_y.append(int(row['impressions']))
+    i = i + 1
+
+total = len(train_y) + len(test_y)
+
+ii = len(train_y)
+
+train_a = np.empty(total - len(train_y))
+train_a.fill(0)
+
+test_a = np.empty(total - len(test_y))
+test_a.fill(0)
+
+a = train_y + list(train_a)
+b = list(test_a) + test_y
+
+plt.plot([],[],color='c', label='Training Impressions', linewidth=5)
+plt.plot([],[],color='y', label='Forecasted Impressions', linewidth=5)
+plt.stackplot(train_x, a, b, colors=['c', 'y'])
+
 plt.xlabel('day')
 plt.ylabel('ad clicks')
-  
+
 plt.title('Click forecasting')
-  
+
 plt.show()
 
